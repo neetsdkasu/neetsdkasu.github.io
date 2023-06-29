9422fd37011846bb4ee80a268786938aacbe7e25
Leonardone <neetsdkasu@gmail.com>
2023-06-30 00:29:20 +0900

    update JobPresetMaximumCost

--- misc/dqwalkhearts/script.ts
+++ misc/dqwalkhearts/script.ts
@@ -300,6 +300,7 @@ const JobPresetMaximumCost: JobMaximumCost[] = [
         ]
     },
     { id: 200, maximumCostList: [
+            { level: 68, maximumCost: 374 },
             { level: 67, maximumCost: 368 },
             { level: 66, maximumCost: 364 },
             { level: 65, maximumCost: 358 },
